529ac9773d2cc128a9b75b204f899f43df4e26e4
Skeleturtle <val742val742@gmail.com>
2022-07-10 17:00:59 -0600

    steppingStone_1

--- js/index.js
+++ js/index.js
@@ -12,33 +12,39 @@ document.querySelector('.sidenav-overlay').setAttribute("style", "block")}
 else{
 document.querySelector('.sidenav-overlay').setAttribute("style", "none");}
 
+
 const listContent = document.querySelector(".listContent");
 const siteContent = document.querySelector(".maincontent");
 
 // parallax setup
-const parallax = (content, picture1="./images/code.jpg", 
-                           picture2="./images/code2.jpg") => {
-    
-    let newContent = (
-    `
+const parallax = function(content, picture1="./images/code.jpg", 
+                           picture2="./images/code2.jpg") {
+    let image1Content = (`
     <div class="parallax-container">
       <div class="parallax">
       <img src="${picture1}"></div>
     </div>
-
+    `)
+    let withContent = (
+    `
     <div class="section blue lighten-4">
     <div class="black-text lighten-text-4">
       <div class="row container">
       <div class="col s12 m10 offset-m1">
+      <div class="currentContent">
         ${content}
       </div>
       </div>
+      </div>
     </div>
-    </div>
+    </div>`)
+    let image2Content = (
+    `
     <div class="parallax-container">
       <div class="parallax"><img src="${picture2}"></div>
     </div>
     `);
+    let newContent = image1Content + withContent + image2Content;
     return newContent;
 };
 
@@ -46,10 +52,13 @@ const parallax = (content, picture1="./images/code.jpg",
 const changePage = function (pageID){
   try{
     const desiredItem = navItemArray.find(navPages => navPages.id == pageID);
-    console.log(desiredItem)
-    siteContent.innerHTML = parallax(desiredItem.content, desiredItem.image1, desiredItem.image2)
-  }catch{
-    console.log("Error returned")
+    // console.log(desiredItem)
+    // console.log(contentBlock)
+    
+    siteContent.innerHTML = parallax(desiredItem.content)
+    
+  }catch(e){
+    console.log("Error returned",e)
   };
 };
 
@@ -69,7 +78,7 @@ const navItemList = navItemArray.map(function (navitem) {
     button.addEventListener("click", ()=>{
         // Event to change 
         changePage(button.id);
-        console.log(button.id);
+        // console.log(button.id);
       
     })
 
@@ -85,15 +94,15 @@ const contentList = navItemArray.map(function (items){
 });
 
 console.log(contentList[0].innerHTML);
-var openPage=contentList[0].innerHTML;
 // var nextPage;
 
 // siteContent.innerHTML = navItemArray;
-siteContent.innerHTML = parallax(openPage);
+siteContent.innerHTML = parallax(contentList[0].innerHTML);
 
 
 // Required Material listeners
 document.addEventListener('DOMContentLoaded', function() {
+
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems);
     var elems = document.querySelectorAll('.sidenav');
