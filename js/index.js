a8f4bfead354760283f101b6e2840c3090d78b54
Skeleturtle <val742val742@gmail.com>
2022-07-10 18:30:30 -0600

    Page Pre-Seperation

--- js/index.js
+++ js/index.js
@@ -13,39 +13,49 @@ else{
 document.querySelector('.sidenav-overlay').setAttribute("style", "none");}
 
 
+
 const listContent = document.querySelector(".listContent");
 const siteContent = document.querySelector(".maincontent");
+const currentContent = document.querySelector(".currentContent");
 
+const refreshSite = function(part2, ...args){
+  siteContent.innerHTML = args[0] + part2 + args[1];}
+  
 // parallax setup
-const parallax = function(content, picture1="./images/code.jpg", 
-                           picture2="./images/code2.jpg") {
-    let image1Content = (`
+const parallax_pt1 = function(picture1="./images/code.jpg") {
+    let Content = (`
     <div class="parallax-container">
       <div class="parallax">
-      <img src="${picture1}"></div>
-    </div>
-    `)
-    let withContent = (
-    `
-    <div class="section blue lighten-4">
-    <div class="black-text lighten-text-4">
-      <div class="row container">
-      <div class="col s12 m10 offset-m1">
-      <div class="currentContent">
-        ${content}
+        <img src=${picture1}>
       </div>
-      </div>
-      </div>
-    </div>
-    </div>`)
-    let image2Content = (
-    `
-    <div class="parallax-container">
-      <div class="parallax"><img src="${picture2}"></div>
     </div>
     `);
-    let newContent = image1Content + withContent + image2Content;
-    return newContent;
+    return Content;}
+
+const parallax_pt2 = function(content) {
+    let Content = (`
+    <div class="section blue lighten-4">
+      <div class="black-text lighten-text-4">
+        <div class="row container">
+          <div class="col s12 m10 offset-m1">
+            <div class="currentContent">
+              ${content}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`);
+    return Content;}
+
+const parallax_pt3 = function(picture2="./images/code2.jpg") {
+    let Content = (`
+    <div class="parallax-container">
+      <div class="parallax">
+        <img src=${picture2}>
+      </div>
+    </div>
+    `);
+    return Content;
 };
 
 // Used to change content of page
@@ -54,13 +64,14 @@ const changePage = function (pageID){
     const desiredItem = navItemArray.find(navPages => navPages.id == pageID);
     // console.log(desiredItem)
     // console.log(contentBlock)
-    
-    siteContent.innerHTML = parallax(desiredItem.content)
+    console.log(desiredItem.content);
+    refreshSite(desiredItem.content, desiredItem.image1, desiredItem.image2,);
     
   }catch(e){
     console.log("Error returned",e)
   };
 };
+
 
 // Create list items from nav array
 const navItemList = navItemArray.map(function (navitem) {
@@ -97,7 +108,7 @@ console.log(contentList[0].innerHTML);
 // var nextPage;
 
 // siteContent.innerHTML = navItemArray;
-siteContent.innerHTML = parallax(contentList[0].innerHTML);
+// siteContent.innerHTML = parallax(contentList[0].innerHTML);
 
 
 // Required Material listeners
@@ -112,3 +123,18 @@ document.addEventListener('DOMContentLoaded', function() {
 navItemList.forEach((navitem) => {
     listContent.append(navitem);
 });
+console.log(parallax_pt1)
+
+var runOnce;
+
+
+//site has ran once trigger
+runOnce = !runOnce;
+  
+if(runOnce){
+  var part1 = parallax_pt1();
+  var part2 = parallax_pt2(contentList[0].innerHTML);
+  var part3 = parallax_pt3();
+  refreshSite(part2, part1, part3);
+  console.log("worked")
+}
